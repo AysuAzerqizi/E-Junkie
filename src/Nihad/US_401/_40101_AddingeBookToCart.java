a25c7e6afd5d1f60728df774fb9c8faf84f90607
N1h4d <168928622+N1h4d@users.noreply.github.com>
2026-01-12 20:48:32 +0400

    Adding user stories

--- src/Nihad/US_401/_40101_AddingeBookToCart.java
+++ src/Nihad/US_401/_40101_AddingeBookToCart.java
@@ -1,4 +1,20 @@
 package Nihad.US_401;
 
-public class _40101_AddingeBookToCart {
+import Nihad.Utility.BaseDriver;
+import Utility.MyFunc;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class _40101_AddingeBookToCart extends BaseDriver {
+    @Test
+    public void Test(){
+        driver.get("https://shopdemo.fatfreeshop.com/");
+
+        WebElement eBook = driver.findElement(By.xpath("//div[@class='column all_tag']//a[text()='Ebook']"));
+        eBook.click();
+
+
+        gozleBagla();
+    }
 }
--- src/Nihad/US_401/_40102_InvalidPromocodeCheck.java
+++ src/Nihad/US_401/_40102_InvalidPromocodeCheck.java
@@ -1,4 +1,12 @@
 package Nihad.US_401;
 
-public class _40102_InvalidPromocodeCheck {
+
+import Nihad.Utility.BaseDriver;
+import org.junit.Test;
+
+public class _40102_InvalidPromocodeCheck  extends BaseDriver {
+    @Test
+    public void Test(){
+
+    }
 }

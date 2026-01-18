354cf6438b62eab17dc0507a1cb234f54ae87ce8
Asef <veliuyevasef@gmail.com>
2026-01-18 20:38:49 +0400

    Asef

---
+++ src/Asef/US_404.java
@@ -0,0 +1,14 @@
+package Asef;
+
+import Utility.BaseDriver;
+import org.junit.Test;
+
+public class US_404 extends BaseDriver {
+
+    @Test
+    public void Test_US_404(){
+
+
+
+    }
+}
---
+++ src/Asef/US_405.java
@@ -0,0 +1,14 @@
+package Asef;
+
+import Utility.BaseDriver;
+import org.junit.Test;
+
+public class US_405 extends BaseDriver {
+
+    @Test
+    public void Test_US_405(){
+
+
+
+    }
+}
